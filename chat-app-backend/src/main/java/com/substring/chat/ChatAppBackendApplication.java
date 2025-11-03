094bcd84577a4f6f120e2390bb6fa2bb1269a6ec
Krishna Awasthi <krishhawasthi28@gmail.com>
2025-11-03 14:05:37 +0530

    Added files

---
+++ chat-app-backend/src/main/java/com/substring/chat/ChatAppBackendApplication.java
@@ -0,0 +1,14 @@
+package com.substring.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppBackendApplication {
+
+	public static void main(String[] args)	{
+		System.setProperty("jdk.tls.client.protocols", "TLSv1.2");
+		SpringApplication.run(ChatAppBackendApplication.class, args);
+	}
+
+}
